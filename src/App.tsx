73a1ea66ee9f07ad97cc8e57d40565a902167164
Suwardhana <suwar_dhana@yahoo.co.id>
2025-09-25 16:06:15 +0700

    init react vite and shadcn

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+
+function App() {
+  return (
+    <Alert>
+      <AlertTitle>Heads up!</AlertTitle>
+      <AlertDescription>
+        You can add components and dependencies to your app using the cli.
+      </AlertDescription>
+    </Alert>
+  );
+}
+
+export default App;
