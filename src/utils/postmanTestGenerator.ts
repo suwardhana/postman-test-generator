3484f28f4b27554d92f4124077a5c5af64679f10
Suwardhana <suwar_dhana@yahoo.co.id>
2025-09-25 16:59:01 +0700

    update test generator by grouping based on parent object

--- src/utils/postmanTestGenerator.ts
+++ src/utils/postmanTestGenerator.ts
@@ -1,29 +1,74 @@
-export function generatePostmanTests(jsonData: any): string {
+interface TestConfig {
+  contentTypeCheck: boolean;
+  statusCode: string;
+}
+
+export function generatePostmanTests(jsonData: any, config: TestConfig): string {
   const tests: string[] = [];
   
-  function generateTestsForObject(obj: any, path = 'jsonData') {
+  // Add config-based tests
+  if (config.contentTypeCheck) {
+    tests.push(`pm.test("Content-Type is application/json", function () {
+    pm.expect(pm.response.headers.get("Content-Type")).to.include("application/json");
+});`);
+  }
+  
+  tests.push(`pm.test("Status code is ${config.statusCode}", function () {
+    pm.response.to.have.status(${config.statusCode});
+});`);
+  
+  function generateTestsForObject(obj: any, path = 'jsonData', parentKey = '') {
     for (const [key, value] of Object.entries(obj)) {
       const currentPath = `${path}.${key}`;
       
       if (value === null) {
-        tests.push(`pm.test("${key} should exist", function () {
+        tests.push(`pm.test("[${key}] should exist", function () {
     pm.expect(${currentPath}).to.not.be.undefined;
 });`);
       } else if (Array.isArray(value)) {
-        tests.push(`pm.test("${key} should be an array", function () {
-    pm.expect(${currentPath}).to.be.an('array');
-});`);
+        const groupTests: string[] = [];
+        groupTests.push(`pm.test("[${key}] should be an array", function () {
+        pm.expect(${key}).to.be.an('array');
+    });`);
+        
         if (value.length > 0 && typeof value[0] === 'object') {
-          generateTestsForObject(value[0], `${currentPath}[0]`);
+          const itemVar = key.slice(0, -1); // Remove 's' from plural
+          groupTests.push(`\n    if (${key}.length > 0) {
+        const ${itemVar} = ${key}[0];`);
+          
+          for (const [itemKey, itemValue] of Object.entries(value[0])) {
+            const type = typeof itemValue;
+            groupTests.push(`        pm.test("[${key}[0].${itemKey}] should be a ${type}", function () {
+            pm.expect(${itemVar}.${itemKey}).to.be.a('${type}');
+        });`);
+          }
+          groupTests.push(`    }`);
         }
+        
+        tests.push(`// 🔹 ${currentPath}\n(function test${key.charAt(0).toUpperCase() + key.slice(1)}(${key}) {\n    ${groupTests.join('\n\n    ')}\n})(${currentPath});`);
       } else if (typeof value === 'object') {
-        tests.push(`pm.test("${key} should be an object", function () {
-    pm.expect(${currentPath}).to.be.an('object');
-});`);
-        generateTestsForObject(value, currentPath);
+        const groupTests: string[] = [];
+        groupTests.push(`pm.test("[${key}] should be an object", function () {
+        pm.expect(${key}).to.be.an('object');
+    });`);
+        
+        for (const [objKey, objValue] of Object.entries(value)) {
+          const type = typeof objValue;
+          if (objValue === null) {
+            groupTests.push(`pm.test("[${key}.${objKey}] should exist", function () {
+        pm.expect(${key}.${objKey}).to.not.be.undefined;
+    });`);
+          } else {
+            groupTests.push(`pm.test("[${key}.${objKey}] should be a ${type}", function () {
+        pm.expect(${key}.${objKey}).to.be.a('${type}');
+    });`);
+          }
+        }
+        
+        tests.push(`// 🔹 ${currentPath}\n(function test${key.charAt(0).toUpperCase() + key.slice(1)}(${key}) {\n    ${groupTests.join('\n\n    ')}\n})(${currentPath});`);
       } else {
         const type = typeof value;
-        tests.push(`pm.test("${key} should be a ${type}", function () {
+        tests.push(`pm.test("[${key}] should be a ${type}", function () {
     pm.expect(${currentPath}).to.be.a('${type}');
 });`);
       }
